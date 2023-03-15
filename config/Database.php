64e7ef28c251ff95ff1b252163c48905eef9fcfe
Gene Holt <geholt@mail.fhsu.edu>
2023-03-15 10:34:42 -0500

    configured DB connect

--- config/Database.php
+++ config/Database.php
@@ -21,10 +21,9 @@
         //DB Connect
         public function connect(){
             $this->conn = null;
-
+            $dsn = "psql:host={$this->host};port={$this->port};dbname={$this->db_name};";
             try{
-                $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name,
-                $this->username, $this->password);
+                $this->conn = new PDO($dsn,$this->username, $this->password);
                 $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             }catch(PDOException $e){
                 echo 'Connection Error: ' . $e->getMessage();
